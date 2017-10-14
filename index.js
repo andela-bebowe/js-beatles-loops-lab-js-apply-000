45cdd77ab8a3d147b5c8b12741eaddc153848c10
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-14 15:54:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   return facts.map(fact => fact + "!!!")
 }
+
+function iLoveTheBeatles(num) {
+  var quote = "I love the Beatles"
+  var arr = []
+  if (num == 7) {
+    for (var i = 0; i < 8; i++) {
+      arr.push(quote)
+    }
+  } else if (num == 17) {
+    arr.push(quote)
+  }
+  return arr;
+}
