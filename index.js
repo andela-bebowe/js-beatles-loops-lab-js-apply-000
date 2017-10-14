4979942e12c4f2f8f053bfc660cb56cb8b5c93cf
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-14 15:48:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function theBeatlesPlay(musicians, instruments) {
+  var duties = [];
+  var length = musicians.length
+  for (var i = 0; i < length; i++) {
+    duties.push(musicians[i] + " plays " + instruments[i])
+  }
+  return duties;
+}
+
+function johnLennonFacts(facts) {
+
+}
